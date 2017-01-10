f604aefee141dea683aa540f682aa79106a7c322
Darek Greenly <git@zielak.pl>
2017-01-10 21:00:36 +0100

    es6 dice

--- src/Dice.js
+++ src/Dice.js
@@ -1,24 +1,14 @@
-(function(global) {
-  'use strict';
 
-  function Die(faces){
-    this.faces = faces;
-  }
-  Die.prototype.roll = function roll(){
-    return Math.ceil( (Math.random() * this.faces-1) + 1);
-  };
-  
-
-
-  function Dice(dice, faces){
+export default class Dice {
+  constructor(dice, faces){
     this.dice = [];
     
     for (let i = 0, len = dice; i < len; i++) {
       this.dice[i] = new Die(faces);
     }
   }
-  
-  Dice.prototype.roll = function roll(){
+
+  roll(){
     let sum = 0;
     let dice = [];
     this.dice.forEach( (die, i) => {
@@ -30,8 +20,17 @@
       sum: sum,
       dice: dice,
     };
-  };
+  }
+}
 
-  global.Dice = Dice;
+class Die {
+  
+  constructor(faces){
+    this.faces = faces;
+  }
+
+  roll(){
+    return Math.ceil( (Math.random() * this.faces-1) + 1);
+  }
 
 }
